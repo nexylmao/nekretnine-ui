9be3b4c3c67287d4a97eab76e9ddeea4a5e12293
Dušan Simić <dusan.simic1810@gmail.com>
2019-04-18 12:34:08 +0200

    Added different search results

--- src/components/SearchResultCard.vue
+++ src/components/SearchResultCard.vue
@@ -1,44 +1,44 @@
 <template>
-	<b-card :img-src="resultCardData.imgSrc" img-left class="search-result-card" :img-height="small ? '150px' : null">
+	<b-card :img-src="data.imgSrc" img-left class="search-result-card" :img-height="small ? '150px' : null">
 		<b-card-text v-if="small">
 			<b-row>
 				<b-col>
-					<RouterLink to="/"><h2>{{ resultCardData.title }}</h2></RouterLink>
+					<RouterLink to="/"><h2>{{ data.title }}</h2></RouterLink>
 					<small>{{ date }}</small><br/>
-					<b-badge v-for="tag in resultCardData.tags" :key="tag" style="margin-right: 5px">{{ tag }}</b-badge>
-					<!-- <h5>{{ resultCardData.address }}, {{ resultCardData.city }}</h5> -->
+					<b-badge v-for="tag in data.tags" :key="tag" style="margin-right: 5px">{{ tag }}</b-badge>
+					<!-- <h5>{{ data.address }}, {{ data.city }}</h5> -->
 				</b-col>
 				<b-col>
 					<small>Cena</small>
-					<h4>{{ resultCardData.price }} din</h4>
+					<h4>{{ data.price }} din</h4>
 					<small>{{ pricePerSquareMeter }}} din/m<sup>2</sup></small>
 				</b-col>
 				<b-col>
 					<small>Kvadratura</small>
-					<h4>{{ resultCardData.area }} m<sup>2</sup></h4>
+					<h4>{{ data.area }} m<sup>2</sup></h4>
 				</b-col>
 			</b-row>
 		</b-card-text>
 		<b-card-text v-else>
 			<b-row>
 				<b-col>
-					<RouterLink to=/><h2>{{ resultCardData.title }}</h2></RouterLink>
+					<RouterLink to=/><h2>{{ data.title }}</h2></RouterLink>
 					<small>{{ date }}</small><br/>
-					<b-badge v-for="tag in resultCardData.tags" :key="tag" style="margin-right: 5px">{{ tag }}</b-badge>
-					<h4>{{ resultCardData.address }}, {{ resultCardData.city }}</h4>
-					<p>{{ resultCardData.shortDescription }}</p>
+					<b-badge v-for="tag in data.tags" :key="tag" style="margin-right: 5px">{{ tag }}</b-badge>
+					<h4>{{ data.address }}, {{ data.city }}</h4>
+					<p>{{ data.shortDescription }}</p>
 				</b-col>
 			</b-row>
 
 			<b-row align-h="between">
 				<b-col>
 					<small>Cena:</small>
-					<h4>{{ resultCardData.price }} din</h4>
+					<h4>{{ data.price }} din</h4>
 					<small>{{ pricePerSquareMeter }} din/m<sup>2</sup></small>
 				</b-col>
 				<b-col>
 					<small>Kvadratura</small>
-					<h4>{{ resultCardData.area }} m<sup>2</sup></h4>
+					<h4>{{ data.area }} m<sup>2</sup></h4>
 				</b-col>
 			</b-row>
 		</b-card-text>
@@ -55,29 +55,18 @@ export default {
 			type: Boolean,
 			default: false,
 			required: false
-		}
-	},
-	data () {
-		return {
-			resultCardData: {
-				imgSrc: 'https://placekitten.com/300/300',
-				title: 'Naslov',
-				createdAt: new Date(),
-				tags: ['dvosoban', 'iznajmljivanje'],
-				address: 'Veljka Petrovica 8',
-				city: 'Novi Sad',
-				shortDescription: 'Kratak opis',
-				price: 41.53,
-				area: 12.54
-			}
+		},
+		data: {
+			type: Object,
+			required: true
 		}
 	},
 	computed: {
 		date () {
-			return moment(this.resultCardData.createdAt).format('DD. MM. Y.')
+			return moment(this.data.createdAt).format('DD. MM. Y.')
 		},
 		pricePerSquareMeter () {
-			return (this.resultCardData.price / this.resultCardData.area).toFixed(2)
+			return (this.data.price / this.data.area).toFixed(2)
 		}
 	}
 }
--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -5,7 +5,7 @@
 		</b-card>
 		<b-row>
 			<b-col cols=8 md=12>
-				<SearchResultCard v-for="_ in [1, 2, 3, 4, 5]" :key="_"/>
+				<SearchResultCard v-for="searchResult in searchResults" :key="searchResult.title" :data="searchResult"/>
 			</b-col>
 			<b-col>
 			</b-col>
@@ -22,6 +22,67 @@ export default {
 	components: {
 		SearchBox,
 		SearchResultCard
+	},
+	data () {
+		return {
+			searchResults: [
+				{
+					title: 'SAVRŠEN PORODIČNI SA POGLEDOM NA HRAM',
+					imgSrc: 'https://nekretnineslike.blob.core.windows.net/profile/5cb23b394914907f102bc00b.png.thumbnail',
+					createdAt: new Date(),
+					tags: ['Trosoban stan', 'Prodaja'],
+					address: 'Neimar',
+					city: 'Beograd',
+					shortDescription: 'UKNJIŽEN Trosoban stan, 80m2 (76m2+ loda + terasa), u održavanoj zgradi sa dva lifta . Odlicno pozicioniran Savršena veze za sve delove grada Odličan...',
+					price: 12980550,
+					area: 80
+				},
+				{
+					title: 'Savršen za Sve Namene u Centru sa Parkingom',
+					imgSrc: 'https://nekretnineslike.blob.core.windows.net/profile/5cb23b394914907f102bc00b.png.thumbnail',
+					createdAt: new Date(),
+					tags: ['Četvorosoban stan', 'Prodaja'],
+					address: 'Takovska, Centar',
+					city: 'Beograd',
+					shortDescription: 'PROSTRAN SVETAO SAVRŠEN STAN U CENTRU - PREPORUKA ! Prelep četvorosoban, održavan porodični stan u zgradi kakve više ne prave, sa dva stana na spratu,...',
+					price: 25371075,
+					area: 110
+				},
+				{
+					title: 'Lux Stan u Lux Zgradi na Savršenoj Lokaciji na Neimaru Za Preporuku!',
+					imgSrc: 'https://nekretnineslike.blob.core.windows.net/profile/5cb23b394914907f102bc00b.png.thumbnail',
+					createdAt: new Date(),
+					tags: ['Trosoban stan', 'Prodaja'],
+					address: 'Hadži Milentijeva, Neimar',
+					city: 'Beograd',
+					shortDescription: 'Savršen, retko dobar dvoiposoban stan u vrlo kvalitetnij zgradi iz 2010.godine. sa pikovanom fasadom, mermernim ulazom, gde se nije stedelo na izolaci...',
+					price: 20060850,
+					area: 68
+				},
+				{
+					title: 'Izuzetan Porodični na Prvom spratu sa Pogledom na Park Uknjižen Odmah Useljiv',
+					imgSrc: 'https://nekretnineslike.blob.core.windows.net/profile/5cb23b394914907f102bc00b.png.thumbnail',
+					createdAt: new Date(),
+					tags: ['Trosoban stan', 'Prodaja'],
+					address: 'Rudo, Zvezdara',
+					city: 'Beograd',
+					shortDescription: 'Savršen, retko dobar dvoiposoban stan u vrlo kvalitetnij zgradi iz 2010.godine. sa pikovanom fasadom, mermernim ulazom, gde se nije stedelo na izolaci...',
+					price: 10502445,
+					area: 92
+				},
+				{
+					title: 'Retko Dobar u Centru u Mirnoj Ulici sa Drvoredom Uknjižen Odmah Useljiv',
+					imgSrc: 'https://nekretnineslike.blob.core.windows.net/profile/5cb23b394914907f102bc00b.png.thumbnail',
+					createdAt: new Date(),
+					tags: ['Trosoban stan', 'Prodaja'],
+					address: 'Đušina, Tašmajdan',
+					city: 'Beograd',
+					shortDescription: 'Izuzetan trosoban porodični stan na Konjarniku kod Aviv Parka, savršeno pozicioniran, funkcionalan, odličnog rasporeda, idealan za porodicu sa malom d...',
+					price: 21004890,
+					area: 94
+				}
+			]
+		}
 	}
 }
 </script>
